4a1178fd83e5394fdc00b41e8ab36af43f8c9062
yama bico <38546765+yamabicoforte@users.noreply.github.com>
2021-09-18 23:47:32 +0900

    :+1:update

--- src/index.js
+++ src/index.js
@@ -16,11 +16,29 @@ const onClickAdd = () => {
   const list = document.createElement("li");
   list.className = "list-row";
   list.appendChild(todoItem);
-  console.log(list);
+  // button
+  const completed = document.createElement("button");
+  completed.className = "completed";
+  completed.innerText = "完了";
+  completed.addEventListener("click", () => {
+    alert("completed");
+  });
 
-  const complate = document
-    .getElementById("imcompleted-list")
-    .appendChild(list);
+  const remove = document.createElement("button");
+  remove.className = "remove";
+  remove.innerText = "削除";
+  remove.addEventListener("click", () => {
+    //削除ボタンの親タグを削除する。
+    const deleteTarget = remove.parentNode.parentNode;
+    console.log(deleteTarget);
+    document.getElementById("imcompleted-list").removeChild(deleteTarget);
+  });
+
+  todoItem.appendChild(todo);
+  todoItem.appendChild(completed);
+  todoItem.appendChild(remove);
+
+  document.getElementById("imcompleted-list").appendChild(list);
 };
 
 document
