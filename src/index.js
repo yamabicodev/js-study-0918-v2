df14ddfcf8c081f43a06e7b6b333963e49e37843
yama bico <38546765+yamabicoforte@users.noreply.github.com>
2021-09-18 09:20:07 +0900

    :+1: add js logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div create
+  const todoItem = document.createElement("div");
+  todoItem.className = "todo-item";
+  // p create
+  const todo = document.createElement("p");
+  todo.className = "todo";
+  todo.innerText = inputText;
+  todoItem.appendChild(todo);
+  // li create
+  const list = document.createElement("li");
+  list.className = "list-row";
+  list.appendChild(todoItem);
+  console.log(list);
+
+  const complate = document
+    .getElementById("imcompleted-list")
+    .appendChild(list);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
