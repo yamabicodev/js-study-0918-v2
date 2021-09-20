cbf73d8f74d2864b8384bb80bbaf832bf50cb8f9
yama bico <38546765+yamabicoforte@users.noreply.github.com>
2021-09-20 11:08:17 +0900

    update

--- src/index.js
+++ src/index.js
@@ -3,50 +3,7 @@ import "./styles.css";
 const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-
-  // div create
-  const todoItem = createTodoItem();
-  // p create
-  const todo = createTodo(inputText);
-  todoItem.append(todo);
-  // completed button
-  //   const completed = createButton("completed", "完了");
-  //   completed.addEventListener("click", () => {
-  //     const todoItem = createTodoItem();
-  //     const todoText = completed.previousSibling.innerText;
-  //     //削除ボタンの親タグを削除する。
-  //     deleteFromList(completed.closest(".list-row"), "incompleted-list");
-  //     // itemにtodoをセット
-  //     todoItem.append(createTodo(todoText));
-  //     const back = createButton("back", "戻す");
-  //     back.addEventListener("click", () => {
-  //       const todoItem = createTodoItem();
-  //       const todoText = back.previousSibling.innerText;
-  //       //削除ボタンの親タグを削除する。
-  //       deleteFromList(back.closest(".list-row"), "completed-list");
-  //       todoItem.append(createTodo(todoText));
-  //       todoItem.append(createRemoveButton());
-  //       // li create
-  //       const list = createTodoList();
-  //       list.appendChild(todoItem);
-  //       document.getElementById("incompleted-list").appendChild(list);
-  //     });
-  //     todoItem.append(back);
-  //     // li create
-  //     const list = createTodoList();
-  //     list.appendChild(todoItem);
-  //     document.getElementById("completed-list").appendChild(list);
-  //   });
-
-  todoItem.appendChild(todo);
-  todoItem.appendChild(createCompletedButton());
-  todoItem.appendChild(createRemoveButton());
-
-  // li create
-  const list = createTodoList();
-  list.appendChild(todoItem);
-
-  document.getElementById("incompleted-list").appendChild(list);
+  addIncompeteList(inputText);
 };
 
 document
@@ -85,43 +42,44 @@ const createButton = (className, buttonName) => {
   return button;
 };
 
-const createRemoveButton = () => {
-  const remove = createButton("remove", "削除");
-  remove.addEventListener("click", () => {
-    //削除ボタンの親タグを削除する。
-    deleteFromList(remove.closest(".list-row"), "incompleted-list");
-  });
-  return remove;
+const getTodoText = (e) => {
+  return e.parentNode.firstElementChild.innerText;
 };
-
-const createCompletedButton = () => {
+const addIncompeteList = (inputText) => {
+  // div create
+  const todoItem = createTodoItem();
+  // p create
+  const todo = createTodo(inputText);
+  todoItem.append(todo);
+  todoItem.appendChild(todo);
+  // completed button
   const completed = createButton("completed", "完了");
   completed.addEventListener("click", () => {
     const todoItem = createTodoItem();
-    const todoText = completed.parentNode.firstElementChild.innerText;
-    //削除ボタンの親タグを削除する。
-    deleteFromList(completed.closest(".list-row"), "incompleted-list");
-    // itemにtodoをセット
-    todoItem.append(createTodo(todoText));
+    todoItem.append(createTodo(getTodoText(completed)));
     const back = createButton("back", "戻す");
     back.addEventListener("click", () => {
-      const todoItem = createTodoItem();
-      const todoText = back.parentNode.firstElementChild.innerText;
-      //削除ボタンの親タグを削除する。
+      addIncompeteList(getTodoText(back));
       deleteFromList(back.closest(".list-row"), "completed-list");
-      todoItem.append(createTodo(todoText));
-      todoItem.append(createCompletedButton());
-      todoItem.append(createRemoveButton());
-      // li create
-      const list = createTodoList();
-      list.appendChild(todoItem);
-      document.getElementById("incompleted-list").appendChild(list);
     });
     todoItem.append(back);
     // li create
     const list = createTodoList();
     list.appendChild(todoItem);
+    deleteFromList(completed.closest(".list-row"), "incompleted-list");
     document.getElementById("completed-list").appendChild(list);
   });
-  return completed;
+  todoItem.append(completed);
+  // remove button
+  const remove = createButton("remove", "削除");
+  remove.addEventListener("click", () => {
+    deleteFromList(remove.closest(".list-row"), "incompleted-list");
+  });
+  todoItem.append(remove);
+
+  // li create
+  const list = createTodoList();
+  list.appendChild(todoItem);
+
+  document.getElementById("incompleted-list").appendChild(list);
 };
